c93eb584cef468a83e78f25863f2644cc2e8588b
Javier Refuerzo <javierrefuerzo@gmail.com>
2025-01-09 18:39:40 -0700

    init commit

---
+++ Sources/SwiftPackageExample/UtilityEnum.swift
@@ -0,0 +1,57 @@
+//
+//  UtilityEnum.swift
+//  Energy Expert for PG&E
+//
+//  Created by Javier Refuerzo on 1/9/25.
+//
+
+enum UtilityEnum: String {
+    
+    case pge = "pge"
+    case sce = "sce"
+    case sce2 = "sce2"
+    
+    //var data:(name: String){
+    var name:(String){
+        switch self {
+        case .pge:
+            return "Pacific Gas and Electric (PG&E)"
+        case .sce,
+                .sce2:
+            return "Southern California Edison (SCE)"
+        }
+    }
+    
+    var nameShort:(String){
+        switch self {
+        case .pge:
+            return "PG&E"
+        case .sce,
+                .sce2:
+            return "SCE"
+        }
+    }
+    
+    
+    
+    public static func getEnum(providerName: String?) -> UtilityEnum? {
+        switch providerName {
+        case "PGE",
+             "pge",
+             "PG&E",
+             "pg&e":
+            return .pge
+        case "SCE",
+             "sce":
+            return .sce
+        case "SCE2",
+            "sce2":
+            return .sce2
+        default:
+            return nil
+        }
+    }
+    
+    
+    
+}
